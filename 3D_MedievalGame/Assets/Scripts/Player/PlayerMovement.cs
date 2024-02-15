12b9dbc83753520839b6c48bd4873339f1f8ce3e
Nicolas Tetsuo Kimura <nicolas.t.kimura@gmail.com>
2024-02-15 19:47:46 -0300

    Animations
    Walk animation

--- 3D_MedievalGame/Assets/Scripts/Player/PlayerMovement.cs
+++ 3D_MedievalGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,30 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
     PlayerInput playerInput;
     CharacterController characterController;
 
+    private Animator animator;
+
     private Vector2 currentMovementInput;
     private Vector3 currentMovement;
     private bool isMovementPressed;
     private float rotationPerFrame = 15f;
 
+    private int isMovingAnimatorHash;
+
     private void Awake()
     {
         playerInput = new PlayerInput();
+        animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
 
         playerInput.Movement.Move.started += onMovement;
         playerInput.Movement.Move.canceled += onMovement;
         playerInput.Movement.Move.performed += onMovement;
+
+        GetAnimatorHash();
     }
 
     private void Update()
     {
         PlayerRotation();
+        Animate();
         characterController.Move(currentMovement * Time.deltaTime);
     }
 
@@ -54,6 +63,18 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void Animate()
+    {
+        if (isMovementPressed && animator.GetBool(isMovingAnimatorHash) == false)
+        {
+            animator.SetBool(isMovingAnimatorHash, true);
+        }
+        else if (!isMovementPressed && animator.GetBool(isMovingAnimatorHash) == true)
+        {
+            animator.SetBool(isMovingAnimatorHash, false);
+        }
+    }
+
     void Gravity()
     {
         if (characterController.isGrounded)
@@ -66,6 +87,11 @@ public class PlayerMovement : MonoBehaviour
             float gravity = -9.8f;
             currentMovement.y += gravity;
         }
+    }
+
+    private void GetAnimatorHash()
+    {
+        isMovingAnimatorHash = Animator.StringToHash("isMoving");
     }
 
     private void OnEnable()
